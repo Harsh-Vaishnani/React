b5490db1049a905f4384e26590755ab250255f06
Harsh-Vaishnani <harshvaishnani8@gmail.com>
2024-07-28 14:55:23 +0530

    React

--- renderinglist/src/App.jsx
+++ renderinglist/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 
 import Array from './Component/Array'
+import Product from './Component/Product'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +10,7 @@ function App() {
   return (
     <>
       <Array/>
+      <Product/>
     </>
   )
 }
--- renderinglist/src/Component/Array.jsx
+++ renderinglist/src/Component/Array.jsx
@@ -1,14 +1,36 @@
-function Array() {
-  const colors = ["Red", "Yellow", "Green", "Blue", "Orange", "Grey"];
-  const colorList = colors.map((color, index) => <li key={index}>{color}</li>);
+import React, { useRef } from 'react';
+
+function ColorSlider() {
+  const colors = ["Red", "Yellow", "LightGreen", "LightBlue", "Orange", "Grey","Aqua","Pink","Violet"];
+  const colorRef = useRef(null);
+  let index = 0;
+
+  const updateColor = () => {
+    if (colorRef.current) {
+      colorRef.current.style.color = colors[index];
+      colorRef.current.textContent = colors[index];
+    }
+  };
+
+  const handleNext = () => {
+    index = (index + 1) % colors.length;
+    updateColor();
+  };
+
+  const handlePrev = () => {
+    index = (index - 1 + colors.length) % colors.length;
+    updateColor();
+  };
 
   return (
-    <>
-      <div>
-        <ul>{colorList}</ul>
+    <div style={{backgroundColor:"black",padding:"10px",textAlign:"center"}}>
+      <div ref={colorRef} style={{ fontSize: '4em', color: colors[0] }}>
+        {colors[0]}
       </div>
-    </>
+      <button onClick={handlePrev} style={{margin:"10px"}}>Prev</button>
+      <button onClick={handleNext} style={{margin:"10px"}}>Next</button>
+    </div>
   );
 }
 
-export default Array;
+export default ColorSlider;
