90512d696713084cb4fa3659c9d93c4615d410ed
Harsh-Vaishnani <harshvaishnani8@gmail.com>
2024-08-01 21:55:02 +0530

    React

--- Cafeu/src/Component/Slider.jsx
+++ Cafeu/src/Component/Slider.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -15,14 +14,55 @@ function Slider() {
         modules={[Navigation, Autoplay]}
         className="mySwiper"
         loop={true}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
+        // autoplay={{
+        //   delay: 2500,
+        //   disableOnInteraction: false,
+        // }}
       >
-        <SwiperSlide>
-          <img src={slider1} />
+        <SwiperSlide className="mt-4">
+          <div className="d-flex container">
+            <div
+              style={{ width: "55%", alignItems: "center" }}
+              className="d-grid"
+            >
+              <div>
+                <p
+                  className="text-danger fw-bold"
+                  style={{
+                    fontSize: "20px",
+                    fontFamily: "Lobster Two,cursive",
+                  }}
+                >
+                  Best In Cafeu
+                </p>
+                <h1
+                  style={{ fontSize: "50px", fontWeight: "600" }}
+                  className="my-4"
+                >
+                  BBQ Chicken{" "}
+                  <a href="" className="fw-bolder" style={{ color: "#c33" }}>
+                    Salad
+                  </a>{" "}
+                  <br />
+                  With Creamy Avocado
+                </h1>
+                <p className="mb-5">
+                  Lorem ipsum dolor sit amet, conscetur adipiscing elit, sed do
+                  eimod <br /> tempor incididunt ut labore et dolore magna
+                  aliqua Quis
+                </p>
+                <div
+                  className="bg-danger text-center text-white rounded-2"
+                  style={{ width: "17%", height: "45px", lineHeight: "45px" }}
+                >
+                  <p>Order Now</p>
+                </div>
+              </div>
+            </div>
+            <img src={slider1} />
+          </div>
         </SwiperSlide>
+
         <SwiperSlide>
           <img src={slider2} />
         </SwiperSlide>
